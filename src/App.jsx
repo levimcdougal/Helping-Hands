98d135af2d5ee492b25b301a368c11061803db57
Stravarix Web <stravarixweb@Stravarixs-MBP.lan>
2026-05-13 12:37:07 -0600

    describe what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import ScrollToTop from './components/ScrollToTop'
-import Home from './pages/Home'
-import Services from './pages/Services'
-import About from './pages/About'
-import Booking from './pages/Booking'
-
-function AnimatedRoutes() {
-  const location = useLocation()
-  return (
-    <div key={location.pathname} className="page-transition">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
-    </div>
-  )
-}
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <div className="page-wrapper">
-        <Navbar />
-        <main>
-          <AnimatedRoutes />
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', fontFamily: 'sans-serif', color: '#333' }}>
+      <p>No content available.</p>
+    </div>
   )
 }
